ff7785d7159158e59a7282f6d97516a080f02a8c
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-08 14:15:42 +0200

    initial commit

---
+++ 99problems/1to10/main.hs
@@ -0,0 +1,37 @@
+myLast :: [a] -> a
+myLast [x] = x
+myLast (_:xs) = myLast xs
+
+myButLast :: [a] -> a
+myButLast [x, _] = x
+myButLast (_:xs) = myButLast xs
+
+elementAt :: [a] -> Int -> a
+elementAt xs n = xs !! (n-1)
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
+
+myReverse :: [a] -> [a]
+myReverse [x] = [x]
+myReverse (x:xs) = myReverse(xs) ++ [x]
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [_] = True
+isPalindrome (x:xs) 
+	| x /= myLast xs = False
+	| otherwise 	 = isPalindrome(dropLast xs)
+
+dropLast :: [a] -> [a]
+dropLast xs = take (myLength(xs) - 1) xs
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten l = case l of
+	Elem e -> [e]
+	List ls -> concat(map flatten ls)
+
+--myFlatten :: 
