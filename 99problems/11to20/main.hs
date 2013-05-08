09205269d422b43af518ac95ab3410b0a3a86232
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-08 15:24:48 +0200

    problem 12 - decodeModified

--- 99problems/11to20/main.hs
+++ 99problems/11to20/main.hs
@@ -6,3 +6,9 @@ encodeModified (x:xs)
 	| size == 1 = Single x : encodeModified xs
 	| otherwise = Multiple (size, x) : encodeModified(dropWhile (x==) xs)
 	where size = 1 + length(takeWhile (x==) xs)
+
+decodeModified :: [EncodeElem a] -> [a]
+decodeModified [] = []
+decodeModified (x:xs) = case x of
+	Single s -> s : decodeModified xs
+	Multiple (n, m) ->  replicate n m ++ decodeModified xs
