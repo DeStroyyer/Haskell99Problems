db0b7c647958ff7ecb77f455b6c9099ac7091d47
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-14 00:52:47 +0200

    problem 26 - combinations

--- 21to28/main.hs
+++ 21to28/main.hs
@@ -1,5 +1,16 @@
+import Data.List
+
 insertAt :: a -> [a] -> Int -> [a]
 insertAt x xs n = take (n-1) xs ++ [x] ++ drop (n-1) xs
 
 range :: Int -> Int -> [Int]
-range m n = [m..n]
+range m n
+	| m > n = []
+	| otherwise =  m : range (m+1) n
+
+-- TODO PROBLEM 23 to 25 !
+
+combinations :: Eq a => Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations _ [] = []
+combinations n (x:xs) = map (x:) (combinations (n-1) xs) ++ combinations n xs
