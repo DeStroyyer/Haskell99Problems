c8e020ac7af8f19770d92bcff7a28a429532ca8f
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-14 11:01:25 +0200

    isPrime update

--- 31to41/main.hs
+++ 31to41/main.hs
@@ -3,5 +3,5 @@ isPrime n
 	| n <= 1 = False
 	| n == 2 = True
 	| even n = False
-	| otherwise = null (filter (==0) (map (mod n) [3,5..l]))
+	| otherwise = all (/= 0) (map (mod n) [3,5..l])
 		where l = ceiling (sqrt (fromIntegral n))
