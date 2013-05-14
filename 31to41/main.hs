6c1d53a029b88bbc2a881325bee6db4abfc91c34
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-14 15:37:01 +0200

    problem 32 - myGcd

--- 31to41/main.hs
+++ 31to41/main.hs
@@ -5,3 +5,7 @@ isPrime n
 	| even n = False
 	| otherwise = all (/= 0) (map (mod n) [3,5..l])
 		where l = ceiling (sqrt (fromIntegral n))
+
+myGcd :: Int -> Int -> Int
+myGcd a 0 = a
+myGcd a b = abs (myGcd b (mod a b))
