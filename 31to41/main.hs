cffadff6bbcb995cb3fcb98c347d36dc5922edb2
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-16 11:20:08 +0200

    problem 35 - primeFactors

--- 31to41/main.hs
+++ 31to41/main.hs
@@ -15,3 +15,17 @@ coprime a b = gcd a b == 1
 
 totient :: Int -> Int
 totient m = length (filter (coprime m) [1..(m-1)])
+
+primeFactors :: Int -> [Int]
+primeFactors n = factorize n primes
+	where primes = filter (isPrime) [1..n]
+
+factorize :: Int -> [Int] -> [Int]
+factorize _ [] = []
+factorize n (x:xs)
+	| n <= 0 = []
+	| n `mod` x == 0 = x : factorize (divInt n x) (x:xs)
+	| otherwise = factorize n xs
+
+divInt :: Int -> Int -> Int
+divInt x y = truncate ((fromIntegral x) / (fromIntegral y))
