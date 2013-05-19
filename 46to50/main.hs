7b9271327f75f38bf7147866b238f91d83621e04
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-19 15:32:20 +0200

    problem 48 - tablen

--- 46to50/main.hs
+++ 46to50/main.hs
@@ -1,3 +1,10 @@
+infixl 4 `or'`
+infixl 4 `nor'`
+infixl 5 `xor'`
+infixl 6 `and'`
+infixl 6 `nand'`
+infixl 3 `equ'`
+
 and' :: Bool -> Bool -> Bool
 and' True True = True
 and' _ _ = False
@@ -21,6 +28,9 @@ impl' a b = a && b || not a
 equ' :: Bool -> Bool -> Bool
 equ' = (==)
 
+table :: (Bool -> Bool -> Bool) -> IO ()
+table = showTable . tableList
+
 tableList :: (Bool -> Bool -> Bool) -> [(Bool, Bool, Bool)]
 tableList f = [(a, b, f a b) | a <- values, b <- values]
 	where values = [True, False]
@@ -29,5 +39,25 @@ showTable :: [(Bool, Bool, Bool)] -> IO ()
 showTable [] = return ()
 showTable ((a,b,c):xs) = putStrLn (show a ++ " " ++ show b ++ " " ++ show c) >> showTable xs
 
-table :: (Bool -> Bool -> Bool) -> IO ()
-table = showTable . tableList
+tablen :: Int -> ([Bool] -> Bool) -> IO ()
+tablen n f = showTablen [xs ++ [f xs] | xs <- combos n]
+
+combos :: Int -> [[Bool]]
+combos n
+	| n == 1 = singles
+	| n == 2 = combos' singles
+	| otherwise = combos' (combos (n - 1))
+	where 
+	singles = [[True], [False]]
+	combos' xs = [a : b | a <- [True, False], b <- xs]
+
+showTablen :: [[Bool]] -> IO ()
+showTablen [] = return ()
+showTablen (x:xs) = showLine x >> showTablen xs
+
+showLine :: [Bool] -> IO ()
+showLine [] = putStrLn ""
+showLine (x:xs) = putStr (show x ++ space) >> showLine xs
+	where space | x == True = "  "
+		    | otherwise = " "
+
